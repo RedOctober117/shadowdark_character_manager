d5990b8e760a9d9e4bb7d85b7dfd0d2e097ee687
telemakos <dbcahilly3@gmail.com>
2024-09-16 18:46:32 -0500

    almost working dyn dispatching

--- src/entitytrait.rs
+++ src/entitytrait.rs
@@ -1,70 +1,54 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{borrow::Borrow, collections::HashMap};
 
-// pub struct AbstractTrait<'a, T> {
-//     key: &'a str,
-//     value: Box<T>,
-// }
-
-// impl<'a, T> AbstractTrait<'a, T> {
-
-//     pub fn new(key: &'a str, value: T) -> Box<Self> {
-//         Box::new(Self {
-//             key,
-//             value: Box::new(value),
-//         })
-//     }
-
-//     pub fn key(&self) -> &str {
-//         self.key
-//     }
-
-//     pub fn value(&self) -> &T {
-//         &self.value
-//     }
-
-// }
-
-pub trait AbstractTrait {
-    // fn new(&self, key: String) -> Self;
-    fn key(&self) -> &str;
+pub struct Player {
+    traits: HashMap<String, Box<Retrievable>>,
 }
 
-pub struct PlayerTrait<T> {
-    key: String,
-    value: Option<T>,
-}
-
-impl<T> PlayerTrait<T> {
-    pub fn new(&self, key: String) -> Arc<Self> {
-        Arc::from(Self {
-            key,
-            value: None,
-        })
+impl Player {
+    pub fn new() -> Self {
+        Self {
+            traits: HashMap::new(),
+        }
     }
 
-    pub fn value(&self) -> &Option<T> {
+    pub fn add_trait(&mut self, key: String, new_trait: Retrievable) {
+        let boxed_trait = Box::from(new_trait);
+        self.traits.insert(key, boxed_trait);
+    }
+
+    pub fn get_trait(&self, key: String) -> Option<&Retrievable> {
+        self.traits.get(&key).map(|v| &**v)
+    }
+}
+
+// pub trait EntityTrait {
+//     fn retrievable(&self) -> impl Fn();
+// }
+
+pub trait Retrievable {
+    fn get(&self) -> impl Fn();
+}
+
+pub struct EntityStat<T> {
+    name: String,
+    value: Box<T>,
+}
+
+impl<T> EntityStat<T> {
+    pub fn new(name: String, value: T) -> Self {
+        Self {
+            name,
+            value: Box::from(value),
+        }
+    }
+
+    pub fn get(&self) -> &T {
         &self.value
     }
 }
 
-impl<T> AbstractTrait for PlayerTrait<T> {
-    fn key(&self) -> &str {
-        &self.key
-    }
-}
-
-pub struct Player<'a> {
-    stats: HashMap<&'a str, &'a dyn AbstractTrait>,
-}
-
-impl<'a> Player<'a> {
-    pub fn new() -> Self {
-        Self {
-            stats: HashMap::new(),
-        }
-    }
-
-    pub fn add_trait(&mut self, new_trait: &'a dyn AbstractTrait) {
-        self.stats.insert(new_trait.key(), new_trait);
+impl<T: Fn()> Retrievable for EntityStat<T> {
+    fn get(&self) -> impl Fn() {
+        self.get()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,5 @@ pub mod talent;
 pub mod entitytrait;
 
 fn main() {
-
-    let mut test_player = Player::new();
-
-    // test_player.add_trait(PlayerTrait::<u16>::new(String::from("trait_1")));
-
-    // let mut entity_traits = EntityTraitMap::new();
-    // entity_traits.add_trait(EntityStat::new("test", 1));
-
-    // println!("{:?}", entity_traits.get_trait("test"));
-    // let mut stat_block = PlayerStats::new();
-
-    // println!("str: {}", stat_block.get_stat(StatsEnum::Strength));
-
-    // stat_block.set_stat(StatsEnum::Strength, 4);
-    // println!("str: {}", stat_block.get_stat(StatsEnum::Strength));
-
-    // for index in 0..6 {
-    //     let offset = 15 - StatsEnum::from(index).to_string().len();
-    //     println!("{}: {:>offset$}", StatsEnum::from(index), stat_block.get_stat(StatsEnum::from(index)));
-    // }
+    let mut player = Player::new();
 }
